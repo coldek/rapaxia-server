7b92f2f892af99311e656b6e74612a16100bb7fd
dukeispie <dukeisagamer@gmail.com>
2021-09-11 01:50:30 -0600

    Config Changes

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,15 @@
 export const Config = {
     secretKey: '99R7yIssZSd9Kkt0UefV',
     defaultExpire: '1d',
+    cdn: {
+        pub: '4a5357b15f04237f571b',
+        secret: 'a4d0212c585a95f386ac'
+    },
     directories: {
         root: `C:\\Users\\dukei\\Desktop\\Rapaxia\\server\\`,
         render: `avatar\\render\\render.py`,
         body: `public\\avatars\\body\\`,
-        headshots: `public\\avatars\\headshots\\`
+        headshots: `public\\avatars\\headshots\\`,
+        imageServer: 'public\\'
     }
 }
