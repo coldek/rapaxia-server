ba0ec953d62b19123c866b9a945accd42d8995e5
dukeispie <dukeisagamer@gmail.com>
2021-10-08 11:19:09 -0600

    Dependency

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Friend } from 'src/db/entities/user/friends.entity';
 import { User } from 'src/db/entities/user/user.entity';
+import { FriendsService } from 'src/friends/friends.service';
 import { UserExistsRule } from './dto/users-exists.decorator';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, UserExistsRule],
+  imports: [TypeOrmModule.forFeature([User, Friend])],
+  providers: [UsersService, UserExistsRule, FriendsService],
   controllers: [UsersController]
 })
 export class UsersModule {}
